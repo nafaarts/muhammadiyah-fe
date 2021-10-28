942e4816c2bab60cc47113a105d15079e0f1d733
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2021-10-29 01:33:25 +0700

    add dist

---
+++ dist/_nuxt/f226e8f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{128:function(n,o,t){"use strict";var w=t(177);t.n(w).a.init()},180:function(n,o,t){t(181),n.exports=t(182)}},[[180,18,1,19]]]);
---
+++ dist/_nuxt/static/1635445989/informasi/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/informasi", {data:[{}],fetch:{},mutations:void 0});
